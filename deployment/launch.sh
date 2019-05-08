0dcdf17bb144babf95b82c6ac61724380d3e472d
张三 <zhangsan@zhangsandeMacBook-Pro.local>
2019-05-08 16:49:38 +1000

    combine couchdb and front end deployments

--- deployment/launch.sh
+++ deployment/launch.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 . ./unimelb-comp90024-group-17-openrc.sh
-echo "Please enter node name:"
-read NODENAME
-echo "Please enter volume name:"
-read VOLUMENAME
-sudo ansible-playbook launch.yml --extra-vars "nodename=$NODENAME volumename=$VOLUMENAME"
+# echo "Please enter node name:"
+# read NODENAME
+# echo "Please enter volume name:"
+# read VOLUMENAME
+#sudo ansible-playbook launch.yml --extra-vars "nodename=$NODENAME volumename=$VOLUMENAME"
+sudo ansible-playbook launch.yml
