87625ad797b3b74b4a539a0491f3b3c472f526c3
xiaoshu <414042407@qq.com>
2019-05-11 21:21:38 +1000

    forntend

---
+++ Deadly Sins/build/environment.js
@@ -0,0 +1,18 @@
+const os = require('os')
+
+const configArgv = JSON.parse(process.env.npm_config_argv)
+const original = configArgv.original.slice(1)
+const stage = original[1] ? original[1].replace(/-/g, '') : ''
+
+let localUrl
+try {
+  const network = os.networkInterfaces()
+  localUrl = network[Object.keys(network)[0]][1].address
+} catch (e) {
+  localUrl = 'localhost'
+}
+localUrl = 'http://' + localUrl + '/'
+
+module.exports = {
+  stage, localUrl
+}
---
+++ Deadly Sins/src/components/SidebarPlugin/SideBar.vue
@@ -0,0 +1,103 @@
+<template>
+    <nav class="navbar navbar-vertical fixed-left navbar-expand-md navbar-light bg-white" id="sidenav-main">
+        <div class="container-fluid">
+
+            <!--Toggler-->
+
+            <router-link class="navbar-brand" to="/">
+                <img :src="logo" class="navbar-brand-img" alt="...">
+            </router-link>
+            <div class="title">
+              <h2>Deadly</h2>
+              <h2>Sins</h2>
+            </div>
+            <slot name="mobile-right">
+                <ul class="nav align-items-center d-md-none">
+
+                    <base-dropdown class="nav-item" position="right">
+                        <a slot="title" class="nav-link" href="#" role="button">
+                            <div class="media align-items-center">
+                              <span class="avatar avatar-sm rounded-circle">
+                                <img alt="Image placeholder" src="img/theme/team-1-800x800.jpg">
+                              </span>
+                            </div>
+                        </a>
+
+                        <div class=" dropdown-header noti-title">
+                            <h6 class="text-overflow m-0">Welcome!</h6>
+                        </div>
+                        <router-link to="/profile" class="dropdown-item">
+                            <i class="ni ni-single-02"></i>
+                            <span>My profile</span>
+                        </router-link>
+                        <router-link to="/profile" class="dropdown-item">
+                            <i class="ni ni-settings-gear-65"></i>
+                            <span>Settings</span>
+                        </router-link>
+                        <router-link to="/profile" class="dropdown-item">
+                            <i class="ni ni-calendar-grid-58"></i>
+                            <span>Activity</span>
+                        </router-link>
+                        <router-link to="/profile" class="dropdown-item">
+                            <i class="ni ni-support-16"></i>
+                            <span>Support</span>
+                        </router-link>
+                        <div class="dropdown-divider"></div>
+                        <a href="#!" class="dropdown-item">
+                            <i class="ni ni-user-run"></i>
+                            <span>Logout</span>
+                        </a>
+                    </base-dropdown>
+                </ul>
+            </slot>
+            <slot></slot>
+            <div v-show="$sidebar.showSidebar" class="navbar-collapse collapse show" id="sidenav-collapse-main">
+
+                <ul class="navbar-nav">
+                    <slot name="links">
+                    </slot>
+                </ul>
+                <!--Divider-->
+                <hr class="my-3">
+
+            </div>
+            </div>
+    </nav>
+</template>
+<script>
+
+  export default {
+    name: 'sidebar',
+    props: {
+      logo: {
+        type: String,
+        default: 'img/brand/sins.jpg',
+        description: 'Sidebar app logo'
+      },
+      autoClose: {
+        type: Boolean,
+        default: true,
+        description:
+          'Whether sidebar should autoclose on mobile when clicking an item'
+      }
+    },
+    provide() {
+      return {
+        autoClose: this.autoClose
+      };
+    },
+    methods: {
+      closeSidebar() {
+        this.$sidebar.displaySidebar(false)
+      },
+      showSidebar() {
+        this.$sidebar.displaySidebar(true)
+      }
+    },
+    beforeDestroy() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.showSidebar = false;
+      }
+    }
+  };
+</script>
